50f313ed316804b9a8c16998f205a7d8d4504152
s <s>
2022-07-19 17:44:52 +0500

    send message to user

--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from config import token
+import datetime
 import json
 
 
@@ -18,7 +19,14 @@ async def get_all_news(message: types.Message):
   with open("news_dict.json") as file:
     news_dict = json.load(file)
 
-  print(news_dict)
+  for k, v in news_dict.items():
+    news = f"{datetime.datetime.fromtimestamp(v['article_date_timestamp'])}\n" \
+      f"{v['article_title']}\n" \
+      f"{v['article_desc']}\n" \
+      f"{v['article_url']}"
+
+    await message.answer(news)
+
 
 
 if __name__ == '__main__':
