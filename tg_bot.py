82eec6a6a7f57f430b254eb03ce8e25a3b235663
s <s>
2022-07-19 17:38:10 +0500

    get_all_news

--- tg_bot.py
+++ tg_bot.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from config import token
+import json
 
 
 
@@ -7,9 +8,17 @@ bot = Bot(token=token)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands='start')
-async def start(message: types.Message):
-  await message.reply('Go to the Dream')
+# @dp.message_handler(commands='start')
+# async def start(message: types.Message):
+#   await message.reply('Go to the Dream')
+
+
+@dp.message_handler(commands='all_news')
+async def get_all_news(message: types.Message):
+  with open("news_dict.json") as file:
+    news_dict = json.load(file)
+
+  print(news_dict)
 
 
 if __name__ == '__main__':
