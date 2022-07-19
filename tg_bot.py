0b4da985a35a25f5b1c27dccf5af4c71ab28b067
s <s>
2022-07-19 20:11:18 +0500

    get last five news

--- tg_bot.py
+++ tg_bot.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.markdown import hbold, hunderline, hcode, hlink
 from config import token
 import datetime
 import json
@@ -20,10 +21,31 @@ async def get_all_news(message: types.Message):
     news_dict = json.load(file)
 
   for k, v in sorted(news_dict.items()):
-    news = f"<b>{datetime.datetime.fromtimestamp(v['article_date_timestamp'])}</b>\n" \
-      f"<u>{v['article_title']}</u>\n" \
-      f"<code>{v['article_desc']}</code>\n" \
-      f"{v['article_url']}"
+    # news = f"<b>{datetime.datetime.fromtimestamp(v['article_date_timestamp'])}</b>\n" \
+    #   f"<u>{v['article_title']}</u>\n" \
+    #   f"<code>{v['article_desc']}</code>\n" \
+    #   f"{v['article_url']}"
+    
+    # news = f"{hbold(datetime.datetime.fromtimestamp(v['article_date_timestamp']))}\n" \
+    #   f"{hunderline(v['article_title'])}\n" \
+    #   f"{hcode(v['article_desc'])}\n" \
+    #   f"{hlink(v['article_title'], v['article_url'])}"
+
+    news = f"{hbold(datetime.datetime.fromtimestamp(v['article_date_timestamp']))}\n" \
+      f"{hlink(v['article_title'], v['article_url'])}"
+
+    await message.answer(news)
+
+
+
+@dp.message_handler(commands='last_five')
+async def get_all_news(message: types.Message):
+  with open("news_dict.json") as file:
+    news_dict = json.load(file)
+
+  for k, v in sorted(news_dict.items())[-5:]:
+    news = f"{hbold(datetime.datetime.fromtimestamp(v['article_date_timestamp']))}\n" \
+      f"{hlink(v['article_title'], v['article_url'])}"
 
     await message.answer(news)
 
